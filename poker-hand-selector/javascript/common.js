25d43e408d979bdb20aefad19ea747327c08a9f5
himWilde <him@kevin-wilde.co.uk>
2010-09-01 20:09:37 +0100

    Checking for straight and flush working, checking for kind a mess will clear up

--- poker-hand-selector/javascript/common.js
+++ poker-hand-selector/javascript/common.js
@@ -15,10 +15,12 @@ function initCardGame()
 		addCardToStage(river[river.length - 1], 'river');
 	}	
 	
-	var cards = finalCards(river,player);
+	var cards = finalCards(player,river);
 	
-	console.log(checkForFlush(cards));
-	console.log(checkForStraight(cards));
+	console.log('Of a Kind: ' + checkForAKind(cards));
+	console.log('Flush: ' + checkForFlush(cards));
+	console.log('Straight: ' + checkForStraight(cards));
+	console.log('High Card: ' + findHighCard(cards));
 }
 
 function newPlayer(name)
@@ -34,7 +36,6 @@ function newPlayer(name)
 function newDeck()
 {
     var values = 14;
-    //var suits = ["h", "h", "h", "h"];
     var suits = ["s", "h", "c", "d"];
     
     var deck = new Array();
@@ -70,12 +71,68 @@ function addCardToStage(card, position)
 	img.setAttribute('width','71');
 	img.setAttribute('height','96');
 	
-	//document.getElementById(position).appendChild(img);
+	document.getElementById(position).appendChild(img);
 }
 
-function findHighCard()
+function findHighCard(cards)
 {
-	//EASY
+	var highCard = 0;
+	
+	if(cards[0].value == 1) highCard = cards[0].value + cards[0].suit;
+	else highCard = cards[6].value + cards[6].suit;
+	
+	return highCard;
+}
+
+function checkForAKind(cards)
+{	
+	var ofAKind = 'false';
+	var three = false;
+	var two = 0;
+	var fullHouse = false;
+	
+	for(var i = 0; i < cards.length; i++)
+	{		
+		if(cards.length - i > 3 && cards[i].value == cards[i+1].value && cards[i].value == cards[i+2].value && cards[i].value == cards[i+3].value)
+		{
+			ofAKind = 'Four ' + cards[i].value + 's';
+			break;
+		}
+		
+		if(cards.length - i > 2 && cards[i].value == cards[i+1].value && cards[i].value == cards[i+2].value)
+		{
+			ofAKind = 'Three ' + cards[i].value + 's';
+			three = true;
+		}
+		
+		if(cards.length - i > 1 && three == false && two == 2 && cards[i].value == cards[i+1].value)
+		{
+			ofAKind = ofAKind + ' & Two ' + cards[i].value + 's';
+			two = 3;			
+		}
+		
+		if(cards.length - i > 1 && three == false && two == 1 && cards[i].value == cards[i+1].value)
+		{
+			ofAKind = ofAKind + ' & Two ' + cards[i].value + 's';
+			two = 2;			
+		}
+		
+		if(cards.length - i > 1 && three == false && two == 0 && cards[i].value == cards[i+1].value)
+		{
+			ofAKind = 'Two ' + cards[i].value + 's';
+			two = 1;			
+		}
+		
+	}
+	
+	//CHECK FOR FULL HOUSE
+	if(three == true && two >= 1)
+	{
+		fullHouse = true;
+		ofAKind = 'Full House!'
+	}
+	
+	return ofAKind;
 }
 
 function checkForFlush(cards)
@@ -93,36 +150,14 @@ function checkForFlush(cards)
 		if(cards[i].suit == 'c') c++;
 	}
 	
+	//CHECK FOR STRAIGHT FLUSH HERE
 	if(h >= 5 || s >= 5 || d >= 5 || c >= 5) return true;
 	else return false;
 }
 
 function checkForStraight(cards)
 {	
-	cards[0].value = 1;
-	cards[1].value = 2;
-	cards[2].value = 3;
-	cards[3].value = 4;
-	cards[4].value = 5;
-	cards[5].value = 6;
-	cards[6].value = 10;
-	
-	var highCard = 0;
-	//Sort Numerically
-    for(var i = 1; i < cards.length; i++)
-    {
-        var pos = i;
-        while(pos != 0){
-            if(cards[pos].value < cards[pos-1].value){
-                temp = cards[pos].value;
-                cards[pos].value = cards[pos-1].value;
-                cards[pos-1].value = temp;
-            }
-            pos--;
-        }
-    }
-
-    //Test for Straight    
+	var highCard = 0; 
     var count = 0;
     
     for(var i = 0; i < cards.length - 1; i++)
@@ -130,24 +165,51 @@ function checkForStraight(cards)
         if(cards[i].value == cards[i+1].value - 1)
         {
         	count++;
-        	highCard = cards[i].value;
+        	highCard = cards[i+1].value;
+        } 
+        else if(cards[i+1].value - 1 > cards[i].value && count < 4)
+        {
+        	count = 0;
+        }
+        
+        if(cards[0].value == 1 && cards[6].value == 13 && count >= 3)
+        {
+        	count++;
+        	highCard = cards[0].value;
         }
     }
-    
-    console.log(highCard);
     
     if(count >= 4)return true
     else return false;
 }
 
-function finalCards(r,p)
+function finalCards(p,r)
 {
 	var c = [ p.cards[0], p.cards[1], r[0], r[1], r[2], r[3], r[4] ];
 	
-	for(var i = 0; i < c.length; i++)
-	{
-		console.log(c[i].value, c[i].suit);
-	}
+	//SORT NUMERICALLY
+    for(var i = 1; i < c.length; i++)
+    {
+        var a = i;
+        while(a != 0){
+            if(c[a].value < c[a-1].value){
+                temp = c[a];
+                c[a] = c[a-1];
+                c[a-1] = temp;
+            }
+            a--;
+        }
+    }
+	
+	console.log(
+		"|" + c[0].value + c[0].suit + "|",
+		"|" + c[1].value + c[1].suit + "|",
+		"|" + c[2].value + c[2].suit + "|",
+		"|" + c[3].value + c[3].suit + "|",
+		"|" + c[4].value + c[4].suit + "|",
+		"|" + c[5].value + c[5].suit + "|",
+		"|" + c[6].value + c[6].suit + "|"
+	);
 	
 	return c;
 }
